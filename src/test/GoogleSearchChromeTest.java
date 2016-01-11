13f940c5820af5f0a14c0751db4a592001006bc0
Maria Sarana <maryia.sarana@gmail.com>
2016-01-11 13:27:53 -0800

    Creating ChromeDriver Test

---
+++ src/test/GoogleSearchChromeTest.java
@@ -0,0 +1,26 @@
+/**
+ *   File Name: GoogleSearchChromeTest.java<br>
+ *
+ *   Sarana, Maria<br>
+ *   
+ *   Created: Jan 11, 2016
+ *   
+ */
+
+package src.test;
+
+/**
+ * GoogleSearchChromeTest //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Sarana, Maria
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class GoogleSearchChromeTest {
+
+}
